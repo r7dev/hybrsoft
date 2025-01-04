5ad7ea6b4201aac2fd68332f0ec5b920a6f014a8
Ricardo Machado <ricardo.machado@outlook.com.br>
2025-01-03 20:49:29 -0400

    Added information card in user details screen.

---
+++ Hybrsoft.EnterpriseManager/Views/User/Details/UserCard.xaml.cs
@@ -0,0 +1,27 @@
+using Hybrsoft.Domain.Dtos;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Hybrsoft.EnterpriseManager.Views
+{
+	public sealed partial class UserCard : UserControl
+	{
+		public UserCard()
+		{
+			this.InitializeComponent();
+		}
+
+		#region Item
+		public UserDto Item
+		{
+			get { return (UserDto)GetValue(ItemProperty); }
+			set { SetValue(ItemProperty, value); }
+		}
+
+		public static readonly DependencyProperty ItemProperty = DependencyProperty.Register(nameof(Item), typeof(UserDto), typeof(PermissionCard), new PropertyMetadata(null));
+		#endregion
+	}
+}
