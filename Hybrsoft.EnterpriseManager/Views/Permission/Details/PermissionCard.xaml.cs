3efe9de81a605a4e4d143d647bf728c2b8a2444f
Ricardo Machado <ricardo.machado@outlook.com.br>
2025-01-03 20:37:05 -0400

    Added information card in permission details screen.

---
+++ Hybrsoft.EnterpriseManager/Views/Permission/Details/PermissionCard.xaml.cs
@@ -0,0 +1,27 @@
+using Hybrsoft.Domain.Dtos;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Hybrsoft.EnterpriseManager.Views
+{
+	public sealed partial class PermissionCard : UserControl
+	{
+		public PermissionCard()
+		{
+			this.InitializeComponent();
+		}
+
+		#region Item
+		public PermissionDto Item
+		{
+			get { return (PermissionDto)GetValue(ItemProperty); }
+			set { SetValue(ItemProperty, value); }
+		}
+
+		public static readonly DependencyProperty ItemProperty = DependencyProperty.Register(nameof(Item), typeof(PermissionDto), typeof(PermissionCard), new PropertyMetadata(null));
+		#endregion
+	}
+}
