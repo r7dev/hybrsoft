2d293d35a70f02a1d32f874d4dc90f3ac2c436bb
Ricardo Machado <ricardo.machado@outlook.com.br>
2025-07-21 12:03:35 -0400

    Added more resources to activation options.

--- Hybrsoft.EnterpriseManager/Services/Infrastructure/ActivationService.cs
+++ Hybrsoft.EnterpriseManager/Services/Infrastructure/ActivationService.cs
@@ -42,6 +42,54 @@ namespace Hybrsoft.EnterpriseManager.Services.Infrastructure
 			{
 				switch (args.Uri.AbsolutePath.ToLowerInvariant())
 				{
+					case "relative":
+					case "relatives":
+						long relativeID = args.Uri.GetInt64Parameter("id");
+						if (relativeID > 0)
+						{
+							return ActivationInfo.Create<RelativeDetailsViewModel>(new RelativeDetailsArgs { RelativeID = relativeID });
+						}
+						return ActivationInfo.Create<RelativesViewModel>(new RelativeListArgs());
+					case "student":
+					case "students":
+						long studentID = args.Uri.GetInt64Parameter("id");
+						if (studentID > 0)
+						{
+							return ActivationInfo.Create<StudentDetailsViewModel>(new StudentDetailsArgs { StudentID = studentID });
+						}
+						return ActivationInfo.Create<StudentsViewModel>(new StudentListArgs());
+					case "classroom":
+					case "classrooms":
+						long classroomID = args.Uri.GetInt64Parameter("id");
+						if (classroomID > 0)
+						{
+							return ActivationInfo.Create<ClassroomDetailsViewModel>(new ClassroomDetailsArgs { ClassroomID = classroomID });
+						}
+						return ActivationInfo.Create<ClassroomsViewModel>(new ClassroomListArgs());
+					case "company":
+					case "companies":
+						long companyID = args.Uri.GetInt64Parameter("id");
+						if (companyID > 0)
+						{
+							return ActivationInfo.Create<CompanyDetailsViewModel>(new CompanyDetailsArgs { CompanyID = companyID });
+						}
+						return ActivationInfo.Create<CompaniesViewModel>(new CompanyListArgs());
+					case "permission":
+					case "permissions":
+						long permissionID = args.Uri.GetInt64Parameter("id");
+						if (permissionID > 0)
+						{
+							return ActivationInfo.Create<PermissionDetailsViewModel>(new PermissionDetailsArgs { PermissionID = permissionID });
+						}
+						return ActivationInfo.Create<PermissionsViewModel>(new PermissionListArgs());
+					case "role":
+					case "roles":
+						long roleID = args.Uri.GetInt64Parameter("id");
+						if (roleID > 0)
+						{
+							return ActivationInfo.Create<RoleDetailsViewModel>(new RoleDetailsArgs { RoleID = roleID });
+						}
+						return ActivationInfo.Create<RolesViewModel>(new RoleListArgs());
 					case "user":
 					case "users":
 						long userID = args.Uri.GetInt64Parameter("id");
